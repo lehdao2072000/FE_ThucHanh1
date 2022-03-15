ba2e7ddc344a26f8356485cad2e8524ce433322f
lehdao2072000 <lethanhdao207@gmail.com>
2022-03-16 00:51:40 +0700

    update ver2

--- validate.js
+++ validate.js
@@ -1,15 +1,15 @@
-var username = document.querySelector('#username');
-var email = document.querySelector('#email');
-var password = document.querySelector('#password');
-var passwordConfirm = document.querySelector('#passwordConfirm');
-var formSubmit = document.querySelector('form');
+const username = document.querySelector('#username');
+const email = document.querySelector('#email');
+const password = document.querySelector('#password');
+const passwordConfirm = document.querySelector('#passwordConfirm');
+const formSubmit = document.querySelector('form');
 
 formSubmit.addEventListener('submit', function(e) {
     e.preventDefault()
-    let checkUser = CheckUser();
-    let checkEmail = CheckEmail();
-    let checkPass = CheckPass();
-    let checkPassConfirm = CheckPassConfirm();
+    const checkUser = checkUser();
+    const checkEmail = checkEmail();
+    const checkPass = checkPass();
+    const checkPassConfirm = checkPassConfirm();
 
     if (checkUser && checkEmail && checkPass && checkPassConfirm) {
         alert('Chúc mừng bạn đăng ký tài khoản thành công');
@@ -17,14 +17,14 @@ formSubmit.addEventListener('submit', function(e) {
 });
 // hiển thị thông báo lỗi
 function showError(key, message) {
-    let parent = key.parentElement;
+    const parent = key.parentElement;
     let formError = parent.querySelector('.form-error');
     parent.classList.add('error');
     formError.innerHTML = message;
 }
 //bắt lỗi đúng
 function showSuccess(key) {
-    let parent = key.parentElement;
+    const parent = key.parentElement;
     let formError = parent.querySelector('.form-error');
     parent.classList.remove('error');
     formError.innerHTML = '';
@@ -34,11 +34,11 @@ function showSuccess(key) {
 
 
 // kiểm tra dữ liệu đầu vào user Tên: chữ hoa chữ thường, không có kí tự đặc biệt, có thể tiếng Việt
-function CheckUser() {
+function checkUser() {
     const regexUser = /^(?=.*[a-z])(?=.*[A-Z])/;
 
     let isCheck = true;
-    if (username.value == '') {
+    if (!username.value) {
         showError(username, 'Không được để trống thông tin');
         isCheck = false;
     } else if (!regexUser.test(username.value)) {
@@ -50,10 +50,10 @@ function CheckUser() {
     return isCheck;
 }
 // kiểm tra email theo chuẩn email
-function CheckEmail() {
+function checkEmail() {
     const regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
     let isCheck = true;
-    if (email.value == '') {
+    if (!email.value) {
         showError(email, 'Không được để trống thông tin')
         isCheck = false;
 
@@ -66,7 +66,7 @@ function CheckEmail() {
     return isCheck;
 }
 // lỗi 
-function CheckMaxMinLenght(key, min, max) {
+function checkMaxMinLenght(key, min, max) {
     if (key.value.lenght < min) {
         showError(key, `Vui lòng nhập tối thiểu ${min} kí tự`);
     }
@@ -78,29 +78,27 @@ function CheckMaxMinLenght(key, min, max) {
 }
 // kiểm tra pass 8-32 kí tự, ít nhất 1 chữ hoa và 1 chữ thường
 
-function CheckPass() {
+function checkPass() {
     const regexPass = /^(?=.*[a-z])(?=.*[A-Z]){8,32}/;
-    //const regexPass = /^(?=.*[a-z])(?=.*[A-Z])/;
     let isCheck = true;
-    if (password.value == '') {
+    if (!password.value) {
         showError(password, 'Không được để trống thông tin');
         isCheck = false;
     } else if (!regexPass.test(password.value)) {
         showError(password, '8-32 kí tự, ít nhất 1 chữ hoa và 1 chữ thường');
         isCheck = false;
     } else {
-
         showSuccess(password);
     }
     return isCheck;
 }
 
-function CheckPassConfirm() {
+function checkPassConfirm() {
     let isCheck = true;
-    if (passwordConfirm.value == '') {
+    if (!passwordConfirm.value) {
         showError(passwordConfirm, 'Không được để trống thông tin');
         isCheck = false;
-    } else if (password.value != passwordConfirm.value) {
+    } else if (password.value !== passwordConfirm.value) {
         showError(passwordConfirm, 'Mật khẩu không khớp')
         isCheck = false;
     } else {
