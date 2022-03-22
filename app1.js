f0633891451b516193c931d54ef90838f8a1b46f
lehdao2072000 <lethanhdao207@gmail.com>
2022-03-22 15:23:29 +0700

    update axios ver1

--- app1.js
+++ app1.js
@@ -1,20 +1,31 @@
 const url = 'https://jsonplaceholder.typicode.com/posts';
 
+//kiểm tra thông tin đầu vào 
+function checkValidate() {
+    let userId = document.querySelector("input[name=userId]");
+    let title = document.querySelector("input[name=title]");
+    let body = document.querySelector("input[name=body]");
+
+    if (!userId.value || !title.value || !body.value) {
+
+        alert("vui lòng không để trống thông tin")
+    }
+}
 
 //lấy thông tin của các bài đăng
 axios.get(url)
     .then(response => {
         document.getElementById("container-posts").innerHTML = '';
-        let dataPost = response.data;
+        var dataPost = response.data;
         let content = '';
-        response.data.forEach(element => {
+        dataPost.forEach(element => {
             content += `
                 <div class="post" id="post_${element.id}">
                 <h3>${element.userId}</h3>
                 <h4>${element.title}</h4>
                 <p>${element.body}</p>
-                <button onclick="updateFormPost(${element.id})">Update</button>
-                <button onclick="deleteFormPost(${element.id})">Delete</button>
+                <button class ="btn btn-update" onclick="updateFormPost(${element.id})">Update</button>
+                <button class ="btn btn-delete" onclick="deleteFormPost(${element.id})">Delete</button>
                 </div>   
                 `
         });
@@ -24,17 +35,16 @@ axios.get(url)
 
 // tạo mới 1 bài đăng
 function createPost() {
-    let userId = document.querySelector("input[name=userId]").value;
-    let title = document.querySelector("input[name=title]").value;
-    let body = document.querySelector("input[name=body]").value;
+    checkValidate();
 
     let formPosts = {
-        userId: userId,
-        title: title,
-        body: body
+        userId: userId.value,
+        title: title.value,
+        body: body.value
     };
     axios.post(url, formPosts)
         .then(response => {
+            console.log(response);
             document.getElementById("container-posts").innerHTML = '';
             let dataPost = response.data;
             let content = '';
@@ -43,8 +53,8 @@ function createPost() {
                     <h3>${dataPost.userId}</h3>
                     <h4>${dataPost.title}</h4>
                     <p>${dataPost.body}</p>
-                    <button onclick="updateFormPost(${dataPost.id})">Update</button>
-                    <button onclick="deleteFormPost(${dataPost.id})">Delete</button>
+                    <button class ="btn btn-update" onclick="updateFormPost(${dataPost.id})">Update</button>
+                    <button class ="btn btn-delete" onclick="deleteFormPost(${dataPost.id})">Delete</button>
                     </div>   
                     `
             document.getElementById("container-posts").innerHTML = content;
@@ -59,17 +69,24 @@ function createPost() {
 function deleteFormPost(id) {
     const delUrl = url + "/" + id;
     axios.delete(delUrl)
-        .then(response => console.log(response.data))
         .then(() => {
             const indexPost = document.querySelector("#post_" + id);
-            indexPost.remove();
+            indexPost.parentNode.removeChild(indexPost);
         })
+        .catch(error => alert(error))
 
 }
 //sửa
 function updateFormPost(id) {
     const updateUrl = url + "/" + id;
-    axios.post(updateUrl)
-        .then(response => console.log(response.data))
+    checkValidate();
+    axios.get(updateUrl)
+        .then(response => {
+            userId.value = response.data.userId;
+            title.value = response.data.title;
+            body.value = response.data.body;
+
+        })
+        .then(() => createPost())
         .catch(error => alert(error))
 }
